2a16189735c5a1fa9048c6ff03258708f5155819
Monica Rodriguez <monik_0491@hotmail.com>
2020-05-09 12:43:29 -0600

    test

---
+++ client/build/precache-manifest.5ad7509460dbbff1da3c39a45f8b24ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e60b111d7eec474bac",
+    "url": "/static/css/main.232b2eb0.chunk.css"
+  },
+  {
+    "revision": "35e60b111d7eec474bac",
+    "url": "/static/js/main.ef491621.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0d5c17320b3994529e0",
+    "url": "/static/js/2.998b6ee3.chunk.js"
+  },
+  {
+    "revision": "72967b1e84e008ef3d2bd0e8e910217f",
+    "url": "/index.html"
+  }
+];
